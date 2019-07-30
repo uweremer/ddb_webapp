710d9a0d55e47e75a503db516042fb415f5ba94f
Uwe Remer-Bollow <URB@SOWI1-ABT1-NB7>
2019-07-30 17:18:58 +0200

    Vanilla Django Project gitignore

---
+++ ddb_webapp/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ddb_webapp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ddb_webapp.settings')
+
+application = get_wsgi_application()
