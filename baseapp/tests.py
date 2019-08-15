98c4435cba3a129f76c7a6bf4d0f7b53e725715f
Uwe Remer-Bollow <URB@SOWI1-ABT1-NB7>
2019-08-15 15:38:11 +0200

    simple test

--- baseapp/tests.py
+++ baseapp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_landingpage(self):
+        """
+        If no landingpage exist, all fine.
+        """
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
