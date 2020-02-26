7f9f83098901685cfd25574e16326d016dc91946
Uwe Remer <uwe.remer@sowi.uni-stuttgart.de>
2020-02-26 14:20:28 +0100

    AND OR und Wildcard in Suche

--- ddb_showcase/forms.py
+++ ddb_showcase/forms.py
@@ -7,19 +7,16 @@ class DateRangeSearchForm(HighlightedModelSearchForm):
     start_date = forms.DateField(required=False)
     end_date = forms.DateField(required=False)
 
-    def search(self):
-        # First, store the SearchQuerySet received from other processing.
-        sqs = super(DateRangeSearchForm, self).search()
 
-        if not self.is_valid():
-            return self.no_query_found()
+#data = {'q': 'Windenergie OR Windkraft', 'start_date': '2017-01-01', 'end_date': '2017-12-31'}
+#a=DateRangeSearchForm(data)
+#a.cleaned_data["q"]
+#a.is_valid()
+#a.cleaned_data["q"]
 
-        # Check to see if a start_date was chosen.
-        if self.cleaned_data['start_date']:
-            sqs = sqs.filter(start_datum__gte=self.cleaned_data['start_date'])
+#sqs1 = a.searchqueryset.auto_query(a.cleaned_data["q"])
+#sqs2 = a.searchqueryset.raw_search(a.cleaned_data["q"])
+#len(sqs1)
+#len(sqs2)
 
-        # Check to see if an end_date was chosen.
-        if self.cleaned_data['end_date']:
-            sqs = sqs.filter(start_datum__lte=self.cleaned_data['end_date'])
-
-        return sqs
+#sqs.result_class()
